5b65d4092ddcb12efb777783e29b7dda17a0d9ef
Ahmad Qadri <arqchicago@gmail.com>
2021-06-13 23:03:52 -0400

    get means of 'n' randomly selected samples from the data distribution

--- clt.R
+++ clt.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-setwd('C:/clt_r')
+setwd('C:/Users/Ahmad/Desktop/Ahmad/python/clt_r')
 
 # start by creating population data that has skewed distribution
 data_df <- data.frame(x=rbeta(10000,2,7)*1000)
@@ -10,3 +10,12 @@ ggplot(data_df, aes(x)) +
 geom_histogram(binwidth=25, fill="yellow", color="red", alpha=0.9) +
 ggtitle("population data distribution")
 ggsave(filename="./output/pop.png", plot=last_plot(), width = 20, height = 15, units='cm')
+
+# create a dataframe of sample means from 500 random samples from the data
+mean_df <- data.frame(mean = numeric())
+
+for (x in 1:500) {
+  sample <- data_df[sample(nrow(data_df), 200),]
+  mean_df <- rbind(mean_df, data.frame(mean = mean(sample)))
+}
+
